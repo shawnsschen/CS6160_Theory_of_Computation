c2f2de103317ae55ebf58e0fe9e4cecba11865c5
Shawn <sschenshawn@gmail.com>
2015-04-24 02:19:03 -0400

    separated main function

--- dlx.py
+++ dlx.py
@@ -307,19 +307,3 @@ class Root(Node):
         size, column = min((column.size, column)
                            for column in self.right_siblings())
         return column
-
-
-# main function
-if __name__ == '__main__':
-    matrix = [
-        'A  B  C  D  E  F'.split(),
-        [1, 1, 0, 0, 0, 0],
-        [1, 0, 1, 0, 0, 0],
-        [0, 0, 1, 1, 0, 0],
-        [0, 1, 0, 1, 0, 0],
-        [0, 0, 0, 0, 1, 1]]
-    mat = ExactCover(matrix)
-    print 'Matrix =\n', mat.root, '\n'
-    for solution in mat.solve():
-        print 'Solutions:\n', sorted(solution), '\n'
-    print mat.num_searches, 'searches'
