38e3f4447e4fa56b15fbd7842b5e3d6483ddff7e
Shawn <sschenshawn@gmail.com>
2015-04-28 00:19:23 -0400

    fixed non-full board bugs

--- solve.py
+++ solve.py
@@ -4,19 +4,7 @@ import puzzle
 import parser
 
 if __name__ == '__main__':
-    origPuzzle = parser.Parser('puzzle_inputs/pentominoes5x12.txt')
-    """
-    bset = []
-    b0 = [[0,0],[1,0],[1,1]]
-    bset.append(b0)
-    b1 = [[0,0],[1,0]]
-    bset.append(b1)
-    b2 = [[0,0],[0,1],[0,2],[1,1]]
-    bset.append(b2)
-    board = [[0,0],[0,1],[0,2],
-             [1,0],[1,1],[1,2],
-             [2,0],[2,1],[2,2]]
-    """
+    origPuzzle = parser.Parser('puzzle_inputs/pentominoes8x8_middle_missing.txt')
     bset = origPuzzle.search()
     board = origPuzzle.board
     bdrows = max([row[0] for row in board]) + 1
@@ -42,19 +30,33 @@ if __name__ == '__main__':
             rowidx = rowbase + offset
             coverMatrix[rowidx][n] = 1
             for pair in coords:
-                colidx = pair[0] * bdcols + pair[1] + len(tiles)
+                colidx = board.index(pair) + len(tiles)
                 coverMatrix[rowidx][colidx] = 1
     # create a name row
     namerow = ['B'+`n` for n in range(len(tiles))]
     namerow += ['N'+`pair[0]`+'_'+`pair[1]` for pair in board]
     coverMatrix = [namerow] + coverMatrix
 
+    indepSol = []
     mat = dlx.ExactCover(coverMatrix)
     solutions = mat.solve()
     if not next(solutions, False):
         print '---- No available solution ----'
+    solcnt = 0
     for solution in solutions:
         graph = graphic.Graph(bdrows, bdcols, solution)
-        print 'Solution:', graph.gen()
-        print '\n'
+        newsol = graph.gen()
+        isIndep = True
+        for _ in range(4):
+            if newsol in indepSol:
+                isIndep = False
+                break
+            # rotate solution matrix 90 degrees clockwise
+            newsol = [list(r) for r in zip(*newsol[::-1])]
+        if isIndep:
+            indepSol.append(newsol)
+            solcnt += 1
+            print 'Solution:', newsol
+            print '\n'
+    print 'found', solcnt, 'independent solutions'
     print mat.num_searches, 'searches'
