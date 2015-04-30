d7195d56da06e391c802f6bfec27ddecc7003c31
Shawn <sschenshawn@gmail.com>
2015-04-30 02:51:09 -0400

    interactive GUI

--- test.py
+++ test.py
@@ -1,31 +1,34 @@
 import solve
 import easygui
 import os
-import threading
-import multiprocessing
 import time
 
 if __name__ == '__main__':
-    FLIP = True
-    ROTATE = True
     choice = easygui.ccbox(msg='Start solving puzzle?\nChoose a puzzle first.', title='Puzzle Solver v1.0', choices=('Continue', 'Cancel'), image=None)
     if choice:
         inputpath = easygui.fileopenbox(title='Choose a puzzle', default=None, filetypes=['*.txt'])
     puzzlename = inputpath.split('/')[-1].split('.')[0]
     outputpath = os.getcwd() + '/results/' + puzzlename
 
+    msg = "Enable flipping and rotation?"
+    title = 'Puzzle Solver v1.0'
+    fieldName = ["Flipping", "Rotation"]
+    fieldValue = []
+    fieldValue = easygui.multenterbox(msg, title, fieldName)
+    FLIP = fieldValue[0]
+    ROTATE = fieldValue[1]
+
     start = time.time()
     solve.solve(inputpath, FLIP, ROTATE)
     end = time.time()
-    print 'Total time: ', end - start
-    """
-    #proc = multiprocessing.Process(target=solve.solve, args=(inputpath, FLIP, ROTATE))
-    proc = threading.Thread(target=solve.solve, args=(inputpath, FLIP, ROTATE))
-    proc.start()
-    time.sleep(3)
+
+    printstr = 'Total time: ' + str(end - start) + ' seconds'
+    msg = "Solutions generated. Please choose one."
+    fieldName = ["Solution number"]
+    fieldValue = []
+    fieldValue = easygui.multenterbox(msg, title, fieldName)
+    imgpath = outputpath + '/' + str(fieldValue[0]) + '.png'
     if os.path.exists(outputpath) and os.listdir(outputpath):
-        easygui.msgbox("pic generated")
+        easygui.msgbox(printstr, title, image=imgpath)
     else:
         easygui.msgbox("nothing found")
-    #proc.terminate()
-    """
