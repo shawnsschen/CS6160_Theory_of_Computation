21639653c6e709a4b69d5af084152f7c9f95b0f2
Shawn <sschenshawn@gmail.com>
2015-05-10 22:50:19 -0400

    added documentation

--- solve.py
+++ solve.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+# Created by: Shawn Chen <schen@virginia.edu>
+#
+# LICENSE
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or(at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details at http://www.gnu.org/copyleft/gpl.html
+#
+# Brief
+# Converts a tiling puzzle problem into an exact cover problem. Does exact
+# cover matrix generation and solution validation and outputs the valid
+# non-isomorphic solutions.
+
+
 import dlx
 import graphic
 import itertools
